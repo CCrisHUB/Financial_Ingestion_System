b6e45b6f94a6ddb163973f801d9b691860a8935e
CVC_Productions <criscoban@gmail.com>
2026-09-08 11:22:59 -0400

    Version: 2.0 fix

--- chase_etl.py
+++ chase_etl.py
@@ -1,3 +1,13 @@
+chase_etl.py
+"""
+Chase ETL Pipeline
+Date: 2026-09-08
+Version: 2.0
+Role: Ingests Chase CSVs, maps transactions, and updates accumulators.
+"""
+__version__ = "2.0"
+__date__ = "2026-09-08"
+
 import os
 import re
 import json
@@ -349,9 +359,7 @@ for cat in CONST_STATIC_MONTHLY_BILLS:
     cat_df = valid_df[valid_df['Mapped_Label'] == cat]
     if not cat_df.empty:
         if cat not in accumulators: accumulators[cat] = {}
-        for _, r in cat_df.iterrows():
-            amt, abs_amt = r['Amount'], r['Abs_Amount']
-            accumulators[cat][latest_month] = accumulators[cat].get(latest_month, 0.0) + (abs_amt if amt < 0 else -abs_amt)
+        accumulators[cat][latest_month] = cat_df.iloc[-1]['Abs_Amount']
 
 # --- GENERATE PAYLOAD ---
 today_str = datetime.now().strftime('%Y-%m-%d')
